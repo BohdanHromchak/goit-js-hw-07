8d730775d758f7035427bf898047d5589df3ee4d
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-17 22:33:16 +0200

    updated

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,3 +1,27 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+// console.log(galleryItems);
 // console.log(basicLightbox);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link"><img src="${preview}" data-src="${original}" alt="${description}" class="gallery__image"></a></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", onClick);
+function onClick(evt) {
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.src}">`
+  );
+  instance.show();
+}
