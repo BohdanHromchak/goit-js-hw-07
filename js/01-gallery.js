6dbbcfe3e28fc5b864fcf701437d7f41720cb6dd
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-17 22:39:25 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,7 +9,7 @@ const gallery = document.querySelector(".gallery");
 const markup = galleryItems
   .map(
     ({ preview, original, description }) =>
-      `<li class="gallery__item"><a class="gallery__link"><img src="${preview}" data-src="${original}" alt="${description}" class="gallery__image"></a></li>`
+      `<div class="gallery__item"><a class="gallery__link" href="${original}"><img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"></a></div>`
   )
   .join("");
 
@@ -17,11 +17,12 @@ gallery.insertAdjacentHTML("beforeend", markup);
 
 gallery.addEventListener("click", onClick);
 function onClick(evt) {
+  evt.preventDefault();
   if (!evt.target.classList.contains("gallery__image")) {
     return;
   }
   const instance = basicLightbox.create(
-    `<img src="${evt.target.dataset.src}">`
+    `<img src="${evt.target.dataset.source}">`
   );
   instance.show();
 }
