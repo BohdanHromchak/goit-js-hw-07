2592bd67655728155ffff46c0467900fe39d7443
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-18 00:43:36 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,8 +8,21 @@ const gallery = document.querySelector(".gallery");
 const markup = galleryItems
   .map(
     ({ preview, original, description }) =>
-      `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" >`
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>`
   )
   .join("");
 
 gallery.insertAdjacentHTML("beforeend", markup);
+gallery.addEventListener("click", onClick);
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
