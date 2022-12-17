33661a28587aeba6d65ede56c5e871d4d86d15ee
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-17 22:54:45 +0200

    updated

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,7 +9,11 @@ const gallery = document.querySelector(".gallery");
 const markup = galleryItems
   .map(
     ({ preview, original, description }) =>
-      `<div class="gallery__item"><a class="gallery__link" href="${original}"><img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"></a></div>`
+      `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+      </a>
+      </div>`
   )
   .join("");
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,13 @@ import { galleryItems } from "./gallery-items.js";
 
 // console.log(galleryItems);
 // console.log(SimpleLightbox);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" >`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
